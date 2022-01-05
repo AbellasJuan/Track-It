05d7a5b111c8cf70e3485aa30a6d54739fc9bdea
abellasjuan <juanvictoraa@gmail.com>
2022-01-05 10:13:27 -0300

    first commit

---
+++ projeto10-trackit/src/App.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export default function App() {
+  return (
+   <Container>
+     <p>start</p>
+   </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: #E5E5E5;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
