ce94cac05a247ebf855ee5c616b1613ba7852e81
thiagoribb <thiribeiro142@gmail.com>
2022-01-13 16:02:17 -0300

    Preparando revisão de código

--- projeto10-trackit/src/globalStyles.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
- 
-const GlobalStyle = createGlobalStyle`
-  body{
-    font-family: 'Lexend Deca', sans-serif;
-  }
-`;
- 
-export default GlobalStyle;
